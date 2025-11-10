fdfef16629cdcf6d1ef292aef6726f5db65e1f6a
Ivana Diaz <ivanadiaz.v@gmail.com>
2025-11-09 22:18:36 -0300

    Practica FEJS

---
+++ js/productos.js
@@ -0,0 +1,7 @@
+export const productos = [
+    { id: "1", nombre: "manzana", descripcion: "fruta roja", precio: 100, imagen: "/img/pulover-gris.jpg", alt: "campera rosa" },
+    { id: "2", nombre: "banana", descripcion: "fruta amarilla", precio: 200, imagen: "/img/pulover-gris.jpg", alt: "campera rosa" },
+    { id: "3", nombre: "leche", descripcion: "bebida", precio: 300, imagen: "/img/pulover-gris.jpg", alt: "campera rosa" },
+    { id: "4", nombre: "pollo", descripcion: "carne blanca", precio: 450, imagen: "/img/pulover-gris.jpg", alt: "campera rosa" },
+    { id: "5", nombre: "fideos", descripcion: "pasta", precio: 520, imagen: "/img/pulover-gris.jpg", alt: "campera rosa" },
+];
