db6763507fe5412771f281908801f2fa2f3038e8
Ivana Diaz <ivanadiaz.v@gmail.com>
2025-11-14 16:53:39 -0300

    Fetch api

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { productos } from "./productos.js";
+// import { productos } from "./productos.js";
 import { agregarAlCarrito } from "./funcionesCarrito.js";
 import { obtenerCarrito } from "./storage.js";
 import { actualizarContador } from "./ui.js";
@@ -9,33 +9,77 @@ document.addEventListener("DOMContentLoaded", () => {
     const carrito = obtenerCarrito();
     actualizarContador(carrito);
 
-    productos.forEach((producto) => {
-        const tarjeta = document.createElement("article");
-        tarjeta.classList.add("tarjeta-producto");
+    fetch("https://fakestoreapi.com/products")
+        .then(res => {
+            if (!res.ok) {
+                throw new Error (`Error HTTP status: ${res.status}`);
+            }
 
-        const imagen = document.createElement("img");
-        imagen.src = `./${producto.imagen}`;
-        imagen.alt = producto.alt;
+            return res.json();
+        })
+        .then(data => {
+            data.forEach((producto) => {
+                const tarjeta = document.createElement("article");
+                tarjeta.classList.add("tarjeta-producto");
 
-        const titulo = document.createElement("h3");
-        titulo.textContent = producto.nombre;
-        
-        const precio = document.createElement("p");
-        precio.textContent = `$${producto.precio}`;
+                const imagen = document.createElement("img");
+                imagen.src = producto.image;
+                imagen.alt = producto.title;
 
-        const boton = document.createElement("button");
-        boton.classList.add("btn");
-        boton.textContent = "Agregar al carrito";
+                const titulo = document.createElement("h3");
+                titulo.textContent = producto.title;
 
-        boton.addEventListener("click", () => {
-            agregarAlCarrito(producto);
+                const precio = document.createElement("p");
+                precio.textContent = producto.price;
+
+                const boton = document.createElement("button");
+                boton.classList.add("btn");
+                boton.textContent = "Agregar al carrito";
+
+                boton.addEventListener("click", () => {
+                    agregarAlCarrito(producto);
+                });
+
+                tarjeta.appendChild(imagen);
+                tarjeta.appendChild(titulo);
+                tarjeta.appendChild(precio);
+                tarjeta.appendChild(boton);
+
+                tarjetas.appendChild(tarjeta);
+            });
+        })
+        .catch(err => {
+            console.log(err);
         });
-
-        tarjeta.appendChild(imagen);
-        tarjeta.appendChild(titulo);
-        tarjeta.appendChild(precio);
-        tarjeta.appendChild(boton);
-
-        tarjetas.appendChild(tarjeta);
-    });
 });
+
+    // productos.forEach((producto) => {
+    //     const tarjeta = document.createElement("article");
+    //     tarjeta.classList.add("tarjeta-producto");
+
+    //     const imagen = document.createElement("img");
+    //     imagen.src = `./${producto.imagen}`;
+    //     imagen.alt = producto.alt;
+
+        // const titulo = document.createElement("h3");
+        // titulo.textContent = producto.nombre;
+        
+        // const precio = document.createElement("p");
+        // precio.textContent = `$${producto.precio}`;
+
+//         const boton = document.createElement("button");
+//         boton.classList.add("btn");
+//         boton.textContent = "Agregar al carrito";
+
+//         boton.addEventListener("click", () => {
+//             agregarAlCarrito(producto);
+//         });
+
+//         tarjeta.appendChild(imagen);
+//         tarjeta.appendChild(titulo);
+//         tarjeta.appendChild(precio);
+//         tarjeta.appendChild(boton);
+
+//         tarjetas.appendChild(tarjeta);
+//     });
+// });
